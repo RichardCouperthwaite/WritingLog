ab136952ff30ac339d552a31b31fb3aaeece3722
Richard Couperthwaite <richardcouperthwaite@tamu.edu>
2018-07-20 14:21:12 -0500

    Added functionality to move to the current session page from the just write button, and fixed so that the comments on the current session will be scrollable but can't be edited.

--- app/src/main/java/edu/tamu/richardcouperthwaite/writinglog/WritingSession.java
+++ app/src/main/java/edu/tamu/richardcouperthwaite/writinglog/WritingSession.java
@@ -1,7 +1,9 @@
 package edu.tamu.richardcouperthwaite.writinglog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class WritingSession extends Activity {
 
@@ -9,6 +11,8 @@ public class WritingSession extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing_session);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 }
