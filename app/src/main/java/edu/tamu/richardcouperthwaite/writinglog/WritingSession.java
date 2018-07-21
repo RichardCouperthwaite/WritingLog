67d70237e69c15577cab3a1875a2f12fbd8c7786
Richard Couperthwaite <richardcouperthwaite@tamu.edu>
2018-07-21 06:28:49 -0500

    Changed the writing session view so that it shows the title bar as well
    Signed-off-by: Richard Couperthwaite <richardcouperthwaite@tamu.edu>

--- app/src/main/java/edu/tamu/richardcouperthwaite/writinglog/WritingSession.java
+++ app/src/main/java/edu/tamu/richardcouperthwaite/writinglog/WritingSession.java
@@ -1,18 +1,13 @@
 package edu.tamu.richardcouperthwaite.writinglog;
 
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.View;
 
-public class WritingSession extends Activity {
+public class WritingSession extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing_session);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-
 }
