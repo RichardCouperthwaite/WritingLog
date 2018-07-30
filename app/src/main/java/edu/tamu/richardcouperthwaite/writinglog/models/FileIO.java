0f7e3b83de69a90abe1ac8d1d5e4b754d3b788fc
Richard Couperthwaite <richardcouperthwaite@tamu.edu>
2018-07-29 21:49:26 -0500

    added the stats class and an empty FileIO class
    Signed-off-by: Richard Couperthwaite <richardcouperthwaite@tamu.edu>

---
+++ app/src/main/java/edu/tamu/richardcouperthwaite/writinglog/models/FileIO.java
@@ -0,0 +1,22 @@
+package edu.tamu.richardcouperthwaite.writinglog.models;
+
+public class FileIO {
+//This class needs to have methods that can handle input and output to three files
+//The three files are:
+//1) projectlist file (JSON)
+//2) stats file (JSON)
+//3) alldata file (csv)
+//
+//The projectlist file will contain a list of all projects that have a structure the same
+//and the Project class. The input and ouput for this file needs to send back a list of all the
+//projects
+//
+//the stats file will contain the data for the current and previous weeks and month. Will need to
+//be able to input and output data to this file as sessions are completed. only need to get the data
+//when the stats tab is opened.
+//
+//The alldata file needs to be a csv type file (for easy export) that records every session in terms
+//of start time, end time, date, project, and final comment. could also consider a comment on how the
+//session went. This will just record all data at the end of each session and add a new line to the end
+//of the file. No output from this file will be necessary at this time.
+}
